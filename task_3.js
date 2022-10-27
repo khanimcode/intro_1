5680288f11ce87f62e4ea75cc81dab8441af1a63
khanimcode <7f3p8e2@code.edu.az>
2022-10-28 00:05:09 +0400

    created

--- task_3.js
+++ task_3.js
@@ -8,7 +8,7 @@ var num3 = 234;
     console.log("Number1 is the smallest");
 }
  
- else if (( num2 < num1) &&( num2 < num3))
+ else if (( num2 < num1) && ( num2 < num3))
 {
     console.log("Number2 is the smallest");
 }
